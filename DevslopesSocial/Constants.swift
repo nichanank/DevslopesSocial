5e97a57750d06901c5ad25d67631973a3c0aab94
Nichanan Kesonpat <nichanan.k@berkeley.edu>
2016-12-05 18:18:38 -0800

    Sign in ui custom classes complete

---
+++ DevslopesSocial/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  DevslopesSocial
+//
+//  Created by Nichanan Kesonpat on 12/5/16.
+//  Copyright © 2016 Nichanan Kesonpat. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
