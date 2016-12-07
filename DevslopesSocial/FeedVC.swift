8a6a111d3b326c35bd097731a03f8041626fef5a
Nichanan Kesonpat <nichanan.k@berkeley.edu>
2016-12-06 21:29:08 -0800

    completed feed table ui & hooked up to backend

--- DevslopesSocial/FeedVC.swift
+++ DevslopesSocial/FeedVC.swift
@@ -7,13 +7,35 @@
 //
 
 import UIKit
+import SwiftKeychainWrapper
+import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +44,11 @@ class FeedVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func signOutTapped(_ sender: AnyObject) {
+        let keychainResult = KeychainWrapper.standard.removeObject(forKey: KEY_UID)
+        print("ID Removed from keychain \(keychainResult)")
+        try! FIRAuth.auth()?.signOut()
+        performSegue(withIdentifier: "goToSignIn", sender: nil)
     }
-    */
 
 }
---
+++ DevslopesSocial/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  DevslopesSocial
+//
+//  Created by Nichanan Kesonpat on 12/6/16.
+//  Copyright © 2016 Nichanan Kesonpat. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLb: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likesLb: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
